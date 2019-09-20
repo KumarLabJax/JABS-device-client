446751427352d7c9b3838fcfaf07468403aff0fa
Glen Beane <glen.beane@jax.org>
2019-09-20 15:31:29 -0400

    add in some capability for checking disk space available

--- system_info.cpp
+++ system_info.cpp
@@ -1,7 +1,9 @@
-#include <sys/sysinfo.h>
 #include <string>
 #include <fstream>
 #include <limits>
+
+#include <sys/sysinfo.h>
+#include <sys/statvfs.h>
 
 #include "system_info.h"
 
@@ -15,8 +17,26 @@ SysInfo::SysInfo(void)
 
 void SysInfo::sample(void)
 {
+    sysinfo(&this->system_info);
     this->update_mem_info();
-    sysinfo(&this->system_info);    
+    
+    // convert the integer value sysinfo gives us to the floating point number we expect
+    this->load = this->system_info.loads[0] / (float)(1 << SI_LOAD_SHIFT);
+    
+    // update disk information
+    for(auto m : this->mount_points) {
+        struct statvfs buf;
+        disk_info di;
+        
+        if (statvfs(m.c_str(), &buf) == 0) {
+            di.capacity = this->blocks_to_gb(buf.f_blocks, buf.f_frsize);
+            di.available = this->blocks_to_gb(buf.f_bavail, buf.f_bsize);
+        } else {
+            di.capacity = 0.0;
+            di.available = 0.0;
+        }
+        this->disk_information[m] = di;
+    }
 }
 
 /*
@@ -45,9 +65,38 @@ void SysInfo::update_mem_info()
     }
 }
 
-float SysInfo::get_load()
+void SysInfo::register_mount(std::string mount)
 {
-    // convert the integer value sysinfo gives us to the floating point number we expect
-    return this->system_info.loads[0] / (float)(1 << SI_LOAD_SHIFT);
+    // TODO check that mount point exists before registering, otherwise throw exception
+    this->mount_points.insert(mount);
+}
+
+std::vector<std::string> SysInfo::get_registered_mounts()
+{
+    std::vector<std::string> v(this->mount_points.begin(), this->mount_points.end());
+    return v;
+}
+
+disk_info SysInfo::get_disk_info(std::string mount)
+{
+
+    disk_info di;
+    
+    auto it = this->disk_information.find(mount);
+    if (it != this->disk_information.end()) {
+        di.capacity = it->second.capacity;
+        di.available = it->second.available;
+        
+    } else {
+        di.capacity = 0.0;
+        di.available = 0.0;
+    }
+    return di;
+}
+
+double SysInfo::blocks_to_gb(fsblkcnt_t blocks, unsigned long bsize)
+{
+    // convert number of filesystem blocks into a size in GB
+    return blocks / (1073741824.0 / bsize);
 }
 
--- system_info.h
+++ system_info.h
@@ -1,4 +1,29 @@
+#include <exception>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 #include <sys/sysinfo.h>
+#include <sys/types.h>
+
+struct disk_info {
+    double capacity;
+    double available;
+};
+
+class DiskRegistrationException: public std::exception {
+  
+  private: 
+      std::string message;
+      
+  public:
+      DiskRegistrationException(std::string msg = "Error registering path"): message(msg) {}
+  
+      virtual const char* what() const throw()
+      {
+          return this->message.c_str();
+      }
+};
 
 /**
  * system information
@@ -9,10 +34,16 @@
  */
 class SysInfo {
     private:
-        struct sysinfo system_info;
+        float load;
         unsigned long mem_available;
         unsigned long mem_total;
+        struct sysinfo system_info;
+        std::set<std::string> mount_points;
+        std::map<std::string, disk_info> disk_information;
+        
         void update_mem_info();
+        double blocks_to_gb(fsblkcnt_t blocks, unsigned long bsize);
+        
 
     public:
         /**
@@ -27,37 +58,60 @@ class SysInfo {
         /**
          * sample system information
          * 
-         * get updated memory usage, load, disk usage
+         * refreshes the view of the system by getting updated memory usage, load,
+         * disk usage, etc
          *
          * @return void
          */
         void sample();
         
         /**
-         * get amount of physical memory
-         *
-         * get the amount of physical RAM in kB
+         * get amount of physical memory in kB
          *
          * @return amount of RAM
          */
         unsigned long get_mem_total() {return this->mem_total;}
-        
-        
+            
         /**
-         * get memory available
-         *
-         * get amount of available RAM in kB
+         * get memory available in kB
          *
          * @return memory available 
          */
         unsigned long get_mem_available() {return this->mem_available;}
-        
-       
         
         /**
          * get 1 minute load average
          *
          * @return floating point load average
          */
-        float get_load();
+        float get_load() {return this->load;}
+        
+        /**
+         * register mount point
+         *
+         * register a mount point so that we will gather information about it (capacity, 
+         * available space)
+         *
+         * @param path string containing path to mount
+         */
+        void register_mount(std::string path);
+        
+        /**
+         * get mount points that have been registered for monitoring
+         *
+         * @return a vector of strings listing all registered mounts
+         */
+        std::vector<std::string> get_registered_mounts();
+        
+        /**
+         * returns information about capacity and available disk space
+         *
+         * this method looks up a mount point, specified as a string, and returns the 
+         * capacity and available disk space in a struct. This mount point must have been
+         * registered prior to the last call to sample(), otherwise reported capacity and
+         * available space will be zero
+         *
+         * @returns a struct containing disk capacity and available space
+         */
+        disk_info get_disk_info(std::string mount);
 };
